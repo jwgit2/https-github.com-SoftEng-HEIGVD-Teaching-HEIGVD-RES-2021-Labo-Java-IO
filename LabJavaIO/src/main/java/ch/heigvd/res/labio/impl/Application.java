ea5908b7f1f681c7b23d61955c7f226562dafac8
jwgit2 <johannwerklegit@gmail.com>
2021-03-17 18:38:32 +0100

    Application test now working hurray

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -90,12 +90,7 @@ public class Application implements IApplication {
         e.printStackTrace();
       }
       if (quote != null) {
-        /* There is a missing piece here!
-         * As you can see, this method handles the first part of the lab. It uses the web service
-         * client to fetch quotes. We have removed a single line from this method. It is a call to
-         * one method provided by this class, which is responsible for storing the content of the
-         * quote in a text file (and for generating the directories based on the tags).
-         */
+
         storeQuote(quote, "quote-" + i+1 + ".utf8");
         LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
         for (String tag : quote.getTags()) {
@@ -157,17 +152,12 @@ public class Application implements IApplication {
    */
   void printFileNames(final Writer writer) {
     IFileExplorer explorer = new DFSFileExplorer();
+
     explorer.explore(new File(WORKSPACE_DIRECTORY), new IFileVisitor() {
       @Override
       public void visit(File file) {
-        /*
-         * There is a missing piece here. Notice how we use an anonymous class here. We provide the implementation
-         * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
-         * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
-         */
-
         try {
-          writer.write(file.getPath());
+          writer.write(file.getPath() + '\n');
           writer.flush();
           writer.close();
         } catch (IOException e) {
